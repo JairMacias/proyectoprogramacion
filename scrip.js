14d3642287fe7e451ca868f75c7224a39bccc6f5
Jair Macias <jairabbad14@gmail.com>
2022-06-12 16:46:43 -0500

    pokemons agua

--- scrip.js
+++
@@ -1,5 +0,0 @@
-const ejemplo =async()=> {
-    let httpEl = await fetch ("https://pokeapi.co/api/v2/pokemon?limit=10");
-    let second = await httpEl.json();
-    console.log ("second", second);
-};
